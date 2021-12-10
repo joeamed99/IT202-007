6f0958431ebb252bdf54304a272b27465af4d28b
Youssef Abdelrehem <yaa25@njit.edu>
2021-12-10 01:55:09 -0500

    update

--- lib/functions.php
+++ lib/functions.php
@@ -297,7 +297,7 @@ function do_bank_action($account1, $account2, $amountChange, $type, $memo){
     $stmt = $db ->prepare("SELECT balance FROM Accounts WHERE id=:id");
     $r = $stmt->execute([ ":id" => $account2]);
     $dest = $stmt->fetch(PDO::FETCH_ASSOC);
-    $dest_total =$dest['balance'];
+    $dest_total = $dest['balance'];
     $dest_total += $amountChange;
 
     $query = "INSERT INTO `Transactions` (`source`, `dest`, `BalanceChange`, `TransactionType`, `memo`, `ExpectedTotal`) 
@@ -350,19 +350,65 @@ function updateAccount($id, $bal){
     return $r;
 }
 
-    function do_bank_extTransfer($account1, $account2, $lastName, $amountChange, $type, $memo){
-        $db = getDB();
-        
-	$stmt = $db ->prepare("SELECT Accounts.id FROM Accounts JOIN Users on Users.id=Accounts.user_id WHERE Accounts.account_number like :account2 AND Users.lastName like :lastName");
-        $r = $stmt->execute([ ":account2" => "%$account2", ":lastName" => "%$lastName%"]);
-        if ($r) {
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        }
-        else {
-           // $e = $stmt->errorInfo();
-        //flash($e[2]);
-	    flash("there was an error with your transaction. Please check the details and try again.");
-        }
+function do_bank_extTransfer($account1, $LastName, $accNum, $amountChange, $type, $memo, $date){
+    $db = getDB();
+    $stmt = $db ->prepare("SELECT balance FROM Accounts WHERE id=:id");
+    $r = $stmt->execute([ ":id" => $account1]);
+    $src =$stmt->fetch(PDO::FETCH_ASSOC);
+    $src_total =$src['balance'];
+
+    if ($src_total < $amountChange){
+        flash ("You do not have enough money available for this transaction");
+        return false;
+    }
+
+    $src_total -= $amountChange;
+
+    $stmt = $db ->prepare("SELECT Accounts.id FROM Accounts JOIN Users on Users.id=Accounts.user_id WHERE Accounts.account_number like :accNum AND Users.LastName like :LastName");
+    $r = $stmt->execute([ ":accNum" => "%$accNum", ":LastName" => "%$LastName%"]);
+    if ($r) {
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    else {
+        $e = $stmt->errorInfo();
+    flash($e[2]);
+    }
+    $account2 = $result['id'];
+    $stmt = $db ->prepare("SELECT balance FROM Accounts WHERE id=:id");
+    $r = $stmt->execute([ ":id" => $account2]);
+    $dest = $stmt->fetch(PDO::FETCH_ASSOC);
+    $dest_total =$dest['balance'];
+    $dest_total += $amountChange;
+    $query = "INSERT INTO `Transactions` (`source`, `dest`, `BalanceChange`, `TransactionType`, `memo`, `ExpectedTotal`)  VALUES(:p1a1, :p1a2, :p1change, :type, :memo, :a1total), 
+            (:p2a1, :p2a2, :p2change, :type, :memo, :a2total)";
+$stmt = $db->prepare($query);
+error_log("query: $query");
+    $stmt->bindValue(":p1a1", $account1);
+    $stmt->bindValue(":p1a2", $account2);
+    $stmt->bindValue(":p1change", $amountChange*-1);
+    $stmt->bindValue(":type", $type);
+    $stmt->bindValue(":memo", $memo);
+    $stmt->bindValue(":a1total", $src_total);
+    // $stmt->bindValue(":date", $date);
+    //flip data for other half of transaction
+    $stmt->bindValue(":p2a1", $account2);
+    $stmt->bindValue(":p2a2", $account1);
+    $stmt->bindValue(":p2change", ($amountChange));
+    $stmt->bindValue(":type", $type);
+    $stmt->bindValue(":memo", $memo);
+    $stmt->bindValue(":a2total", $dest_total);
+    // $stmt->bindValue(":date", $date);
+      $r = $stmt->execute();
+      if($r){
+        updateAccount($account1, $src_total, $date);
+        updateAccount($account2, $dest_total, $date);
+      }
+      else{
+          $e = $stmt->errorInfo();
+          flash("Error creating: " . var_export($e, true));
+      }
+      return $r;
+  }
     function getWorldID(){
         $db = getDB();
         $q = "SELECT id from Accounts WHERE account_number='000000000000'";
@@ -376,12 +422,12 @@ function updateAccount($id, $bal){
         
         
         
-        if (!isset($v)) {
-                echo "";
-                return;
-            }
-            echo htmlspecialchars($v, ENT_QUOTES, "UTF-8");
-        }
+        // if (!isset($v)) {
+        //         echo "";
+        //         return;
+        //     }
+        //     echo htmlspecialchars($v, ENT_QUOTES, "UTF-8");
+        // }
         function getAccount($n){
             switch ($n) {
                 case "checking":
